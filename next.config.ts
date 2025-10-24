2933d822977a8830a91d4b7c316c7e55a170e454
Automan1218 <2984335643@qq.com>
2025-10-24 16:11:10 +0800

    update next config

--- next.config.ts
+++ next.config.ts
@@ -16,9 +16,6 @@ const nextConfig: NextConfig = {
             },
         ]
     },
-    experimental: {
-        dynamicIO: true,  // 添加这行
-    },
     transpilePackages: [
         'antd',
         '@ant-design/pro-components',
